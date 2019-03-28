e2b89950e6e13c1ca5f49e5970976afbaf8374c1
Florian Grzonka <33416573+grzonka@users.noreply.github.com>
2019-03-28 23:30:03 +0100

    comÃpleted elo.py based on inputs in matches.txt

--- elo.py
+++ elo.py
@@ -1,11 +1,61 @@
-import sys
+players = []
+k_value = 32
+
+
+def read_players():
+    line = f.readline()
+    while line != '\n':
+        players.append([line.replace("\n", "")])
+        line = f.readline()
+    for player in players:
+        player.append(1200)
+    line = f.readline()
+    while line != '':
+        calc_new_elo(line.split(", ")[0], line.split(", ")[1].replace("\n", ""))
+        line = f.readline()
+
+def print_players():
+    sorted_list = sorted(players, key=lambda l: l[1], reverse=True)
+    print("The current Elo ratings are:")
+    for p in range(len(sorted_list)):
+        print(sorted_list[p][0].ljust(12) + str(round(sorted_list[p][1])).rjust(4))
 
 
 
-def print_name(name):
-    print(name)
+def calc_new_elo(winner, looser):
+    """
+    :param winner: string
+    :param looser: string
+    :return: new winner elo, new looser elo
+    """
+    for x in range(len(players)):
+        if winner == players[x][0]:
+            p1_elo = players[x][1]
+            p1_index = x
+        if looser == players[x][0]:
+            p2_elo = players[x][1]
+            p2_index = x
+
+    #print(p1_elo)
+    #print(p2_elo)
+
+    rating_change = k_value*(1-expected_score(p1_elo, p2_elo))
+    #print(rating_change)
+    p1_elo = p1_elo + rating_change
+    p2_elo = p2_elo - rating_change
+
+    players[p1_index][1] = p1_elo
+    players[p2_index][1] = p2_elo
+
+def expected_score(rating1, rating2):
+    ea = 1/(1+10**((rating2-rating1)/400))
+    #print("Player one should win by: " + str(ea))
+    return ea
+
 
 if __name__ == '__main__':
-    #print("Number of arguments:" + str(len(sys.argv))) + " arguments."
-    #print("Arguments are: " + str(sys.argv))
-    print_name(sys.argv[1])
+    with open('matches.txt', 'r') as f:
+        read_players()
+        # print_players()
+        # calc_new_elo(Sharon, "Vinh")
+        print_players()
